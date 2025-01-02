61681ea93873227ecb1fc01691dd9d037dd373ec
sailalith192 <sailalith192@gmail.com>
2025-01-02 23:14:41 +0530

    Comparision Operators

---
+++ pythonbasics.py/ComparisionOperator.py
@@ -0,0 +1,35 @@
+# Equal to (==)
+x = 5
+y = 10
+sai = x==y
+print(sai)
+
+# Not equal to (!=)
+a = 7
+b = 7
+result = a!=b
+print(result)
+
+# Greater than(>)
+num1 = 15
+num2 = 10
+result = num1 > num2
+print(result)
+
+# Less than (<)
+s = 4
+t = 8
+result = s<t
+print(result)
+
+# Greater than or equal to(>=)
+m = 5
+n = 3
+result = m>=n
+print(result)
+
+# Less than or equal to(<=)
+r = 10
+s = 15
+sai = r<=s
+print(sai)
