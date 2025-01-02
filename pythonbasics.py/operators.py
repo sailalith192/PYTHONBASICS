c2b031afc542f00ab4e62fad31389d51a1dae996
sailalith192 <sailalith192@gmail.com>
2025-01-02 22:46:31 +0530

    operators

--- pythonbasics.py/operators.py
+++ pythonbasics.py/operators.py
@@ -45,3 +45,51 @@ x = 10
 y = 3
 result = x//y
 print(result)
+
+# Assignment Operators
+x = 10
+print(x)
+
+a = 10
+b = 20
+a += b
+print(a)
+
+a = 10
+a += 20
+print(a)
+
+# Add and Assign (+=)
+y = 5
+y += 3
+print(y)
+
+# Subract and Assign (-=)
+z = 8
+z -= 2
+print(z)
+
+# Mul and Assign (*=)
+s = 4
+s *= 2
+print(s)
+
+# Divide and Assign (/=)
+a = 10
+a /= 2
+print(a)
+
+# Integer divide and Assign (//=)
+c = 15
+c //= 4
+print(c)
+
+# Modulo and Assign(%=)
+e = 12
+e %= 5
+print(e)
+
+# Expo and Assign (**=)
+f = 2
+f **= 3
+print(f)
