3ff7c07b984a2b7461e1e86e6945c892e340d272
sailalith192 <sailalith192@gmail.com>
2025-01-02 20:14:05 +0530

    first commit

---
+++ pythonbasics.py/python.py
@@ -0,0 +1,19 @@
+#Typecasting to int
+num_int = int(5.7)
+print(num_int)
+
+#Typecasting to float
+num_float = float("3.14")
+print(num_float)
+
+#Typecasting tp string
+message = str(42)
+print(message)
+
+#Typecasting to boolean
+is_true = bool(1)
+is_false = bool(0)
+
+a = "sai lalith"
+a = str(a)
+print(a)
