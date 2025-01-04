54a22a70e373e25c86841deac4dd307187ee66b3
sailalith192 <sailalith192@gmail.com>
2025-01-04 20:39:05 +0530

    Logical Operators

---
+++ pythonbasics.py/logicalOperators.py
@@ -0,0 +1,26 @@
+# Logical Operators: AND
+x = True
+y = False
+result = x and y
+print(result)
+
+# Logical Operators: OR
+x = True
+y = False
+result = x or y
+print(result)
+
+x = True
+y = True
+sai = x or y
+print(sai)
+
+x = False
+y = False
+sai = x or y
+print(sai)
+
+# LOgical Operators: NOT
+p = False
+hey = not p
+print(hey)
