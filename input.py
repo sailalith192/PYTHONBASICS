b23f526fca1e85935818e999dc3062fc96e62fd3
SAI lalith <sailalith192@gmail.com>
2025-01-05 18:04:43 +0530

    input

---
+++ input.py
@@ -0,0 +1,12 @@
+# input statement
+name = input("Enter your name: ")
+age = int(input("Enter yuor age: "))
+print("Hello, " + name + "! You are" , age , "years old.")
+
+
+# Output statements
+
+num1 = int(input("give num1 value: "))
+num2 = int(input("give num2 value: "))
+print(num1 , num2 , sep="," , end="the end.")
+
