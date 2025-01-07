347d2ec3d239bf315ecaa5a10056a227497fe929
SAI lalith <sailalith192@gmail.com>
2025-01-07 20:19:43 +0530

    Area of a circle

---
+++ pythonbasics.py/Area of circle.py
@@ -0,0 +1,4 @@
+radius = int(input("radius: "))
+area = 3.14*(radius**2) #exponential by radius*radius *2
+print("Area of a circle:", area,sep="")
+print(f"Area of a circle:{area}")
---
+++ pythonbasics.py/examples.py
@@ -0,0 +1,4 @@
+# Finding the sum of two numbers
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print("Sum:", num1+num2,sep="")
