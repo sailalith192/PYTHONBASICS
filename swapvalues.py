78a31b3bbfa3531c94077dbd72da8602fb517d42
SAI lalith <sailalith192@gmail.com>
2025-01-12 17:34:59 +0530

    swap

---
+++ swapvalues.py
@@ -0,0 +1,12 @@
+# Swap two Values using temporary variable
+a = int(input("give a value: "))
+b = int(input("give b value: "))
+
+temp = a
+a = b
+b = temp
+print(f"After Swapping a is:{a}") 
+print(f"After Swapping b is:{b}") 
+
+
+
